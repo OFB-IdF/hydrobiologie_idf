a85c7cb234a62cc24b7ccf9a250b67d8023f2bba
cedric.mondy <cedric.mondy@ofb.gouv.fr>
2023-06-05 12:28:10 +0200

    Corrige la liste des dépendances

--- install_dep.R
+++ install_dep.R
@@ -1,2 +1,2 @@
-install.packages(c("devtools", "sf", "dplyr", "rmapshaper", "openxlsx2", "janitor", "purrr", "tidyr", "stringr", "ggplot2", "patchwork", "knitr", "lubridate", "leaflet", "mapview", "leafpop", "leaflet.extras", "import"))
+install.packages(c("devtools", "sf", "dplyr", "rmapshaper", "openxlsx2", "janitor", "purrr", "tidyr", "stringr", "ggplot2", "patchwork", "knitr", "lubridate", "leaflet", "mapview", "leafpop", "leaflet.extras", "import", "plotly"))
 devtools::install_github("OFB-IdF/hubeau")
